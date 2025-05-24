4faa117c3fbd0b3f9f20aa8a831bbda31839f230
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-05-24 17:44:26 -0300

    :tada: Commit Inicial: Criação da Package Model

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,50 @@
+package com.generation.lojagames.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
